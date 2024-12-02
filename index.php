8756ba5b01d97f932cc4ab34faa246875143e5e7
Shail Shah <33388945+ssshah2131@users.noreply.github.com>
2024-12-02 17:47:37 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>POC</title>
+</head>
+<body>
+    <h1>Hello Team! This is POC created for responsible disclosure by Shail Shah</h1>
+    <?php
+        
+    ?>
+</body>
+</html>
